887edc5d8db84ef7939c8d4a064d29e43af2f6fb
Martin Palmer <martin.palmer@almmechanics.com>
2018-11-22 11:28:37 +0000

    Added generic tests to call pylint

---
+++ test_pylint.py
@@ -0,0 +1,25 @@
+"""pylint dynamic pytest tool"""
+
+import glob
+import subprocess
+import json
+import sys
+import getopt
+from collections import Counter
+import pytest
+
+location = '.'
+analysisfiles = glob.glob(location +'/**/*.py', recursive=True)
+@pytest.mark.parametrize('filepath',analysisfiles )
+
+def test_eval(filepath):
+    print('creating tests for file {}'.format(filepath))
+    lint_json = []
+
+    proc = subprocess.Popen("pylint "+ filepath + " -f json --persistent=n --score=y",
+                            stdout=subprocess.PIPE, shell=True)
+    (out, _err) = proc.communicate()
+    if out and  out.strip():
+        lint_json = json.loads(out)
+
+    assert len(lint_json) == 0 
