3afb2ea00f9bd61dd68f0bfa38a6a397eba55ab7
Martin Palmer <martin.palmer@almmechanics.com>
2018-11-22 11:38:52 +0000

    changed to pytest summary

--- test_pylint.py
+++ test_pylint.py
@@ -7,12 +7,14 @@ import sys
 import getopt
 from collections import Counter
 import pytest
+import warnings
+
 
 location = '.'
 analysisfiles = glob.glob(location +'/**/*.py', recursive=True)
 @pytest.mark.parametrize('filepath',analysisfiles )
 
-def test_eval(filepath):
+def test_file_has_no_pylint_errors(filepath):
     print('creating tests for file {}'.format(filepath))
     lint_json = []
 
@@ -21,5 +23,10 @@ def test_eval(filepath):
     (out, _err) = proc.communicate()
     if out and  out.strip():
         lint_json = json.loads(out)
+
+    if lint_json is not None:
+        for lint in lint_json:
+            warnings.warn('Line: {} is {}[{}] in {}.py\n'.format(
+                lint['line'], lint['message'], lint['message-id'], lint['module']))
 
     assert len(lint_json) == 0 
